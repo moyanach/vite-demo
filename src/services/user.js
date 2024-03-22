dd9f90c787d7ef04bd4425d45e7e46a98a9f5854
zhoushijie <977816193@qq.com>
2024-03-22 12:01:30 +0800

    修改 format

--- src/services/user.js
+++ src/services/user.js
@@ -1,3 +1,6 @@
 import reuqests from "../utils/requests";
 
-export const userLogin = async (data) => reuqests.post('/cmdb/v1/user/login/', data, {headers: { 'content-type': 'application/x-www-form-urlencoded' }})
+export const userLogin = async (data) =>
+	reuqests.post("/cmdb/v1/user/login/", data, {
+		headers: { "content-type": "application/x-www-form-urlencoded" },
+	});
